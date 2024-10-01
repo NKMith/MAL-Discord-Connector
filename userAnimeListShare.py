51b83b34d0650332df04fd3bcc2e608c09ac29c1
Geon Woo Kim <gwk1@sfu.ca>
2024-10-01 16:39:53 -0700

    userAnimeListShare can now fetch public user anime lists

--- userAnimeListShare.py
+++ userAnimeListShare.py
@@ -1,17 +1,26 @@
 import requests
+from env import *
 
 # Can't use Jikan because getUserAnimeList was discontinued
 class userAnimeListShare():
     def __init__(self, username) -> None:
         self.username = username
 
+    def get_authorization_header(self):
+        return {'X-MAL-CLIENT-ID': str(MAL_CLIENT_ID)}
+    
+    def get_query_parameters(self):
+        return {
+            'limit' : 10,
+            'sort' : 'list_score'
+        }
+    
+        # status, sort, limit, offset
+
     def get_request(self):
-        # curl 'https://api.myanimelist.net/v2/users/@me/animelist?fields=list_status&limit=4' \
-        # -H 'Authorization: Bearer YOUR_TOKEN'
-
         # https://myanimelist.net/apiconfig/references/api/v2#operation/users_user_id_animelist_get
-
-        request = requests.get(f"https://api.myanimelist.net/v2/users/{self.username}/animelist?fields=list_status&limit=4")
+        urlstr = f"https://api.myanimelist.net/v2/users/{self.username}/animelist"
+        request = requests.get(urlstr, params=self.get_query_parameters(), headers=self.get_authorization_header())
         return request.json()
     
 
