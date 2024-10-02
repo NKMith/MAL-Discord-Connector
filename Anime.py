66235085f5e7d709507b0590fa69c1bcad648e9c
Geon Woo Kim <gwk1@sfu.ca>
2024-10-02 09:36:02 -0700

    Created anime.py to represent anime objects

---
+++ Anime.py
@@ -0,0 +1,33 @@
+class Anime():
+    def __init__(self, animedict) -> None:
+        self.animedict = animedict
+        self.url = animedict['url']
+        self.id = animedict['mal_id']
+        self.title = animedict['title']
+        self.large_image_url = animedict['images']['jpg']['large_image_url']
+        self.genrenames :list = self.get_genre_list()
+
+    def get_genre_list(self, anime) -> list:
+        """Given an anime, returns a Python list of genres it has"""
+        lst = []
+        for genre in anime["genres"]:
+            lst.append(genre["name"])
+        return lst
+    
+    def get_nice_print_genres(self, anime) -> str:
+        """
+        PRE: self.strgenres isn't empty
+        Given an anime, returns a string of its genres nice to print
+        """
+        toRet = self.genrenames[0]
+        for i in range(1, len(self.genrenames)):
+            toRet += ", " + self.genrenames[i]
+        return toRet
+    
+
+
+class Anime_UserList(Anime):
+    def __init__(self, animedict, list_status) -> None:
+        super().__init__(animedict)
+        self.user_score = list_status['score']
+        self.status = list_status['status']
